ea1db8c2b67b9c715c5df76a9eb3c92427ba64e6
Jorge Oliveira <jorgeedsonjprn@gmail.com>
2022-05-12 19:06:31 -0300

    configurações iniciais como rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import Routes from "../src/_config/routes";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    
+      <Routes />
+    </BrowserRouter>
   );
 }
 
---
+++ src/features/home/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Paulomix = styled.div`
+  background-image: tomato;
+  color: tomato;
+  font-size: 2rem;
+  font-weight: bold;
+  `;
+
+function Home() {
+  return (
+    
+     
+      <Paulomix>
+            Paulo mix         
+
+      </Paulomix>
+      
+    
+  );
+}
+
+export default Home;
